907a0014cdb2767df38bfb61768e6b9ebec03931
anderson-bassan <bassan.programador@gmail.com>
2022-08-18 19:25:34 -0300

    Create Maven Artifact with Junit, and Surefire
    Sets projects structures and assures that everything is working properly.

---
+++ learn-mockito/src/main/java/com/learnmokito/App.java
@@ -0,0 +1,13 @@
+package com.learnmokito;
+
+/**
+ * @author Anderson Bassan
+ *
+ */
+
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ learn-mockito/src/test/java/com/learnmokito/AppTest.java
@@ -0,0 +1,11 @@
+package com.learnmokito;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+	@Test
+	void testAdd() {
+	}
+}
